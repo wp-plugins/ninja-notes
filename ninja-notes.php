7320c745d656f54056f781270d769a6522de4c7f
Code-Ninja <Code-Ninja@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-10 08:31:56 +0000

    ;o)
    git-svn-id: https://plugins.svn.wordpress.org/ninja-notes/trunk@542272 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ninja-notes.php
+++ ninja-notes.php
@@ -1,7 +1,7 @@
 <?
 /*
 Plugin Name: Ninja Notes
-version: 0.1.2
+version: 0.1.3
 Plugin URI: http://www.code-ninja.co.uk/
 Description: NOTES App for keeping track of various things
 Author: Code Ninja
