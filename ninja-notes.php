bad4556c3175545dbdc1fb5f301af9d49bbb16a6
Code-Ninja <Code-Ninja@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-15 09:02:45 +0000

    Fixing Typo
    git-svn-id: https://plugins.svn.wordpress.org/ninja-notes/trunk@966246 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ninja-notes.php
+++ ninja-notes.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Ninja Notes
-version: 1.1
+version: 1.1.3
 Plugin URI: http://www.code-ninja.co.uk/
 Description: NOTES App for keeping track of various things
 Author: Code Ninja
